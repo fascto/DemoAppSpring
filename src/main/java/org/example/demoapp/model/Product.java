98a553153f3dfa175a60bf10c816381cadadc9b2
fascto <fascto.05@gmail.com>
2025-04-05 10:55:34 -0300

    feat: persist product data using H2 database with Spring JPA

--- src/main/java/org/example/demoapp/model/Product.java
+++ src/main/java/org/example/demoapp/model/Product.java
@@ -1,14 +1,23 @@
 package org.example.demoapp.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
+@Entity
 public class Product {
+
+    @Id
     private int id;
     private String name;
     private double price;
+
+    public Product() {
+
+    }
 
     // Elimina TODOS los getters/setters manuales
     // Lombok los genera automáticamente con @Data
--- src/main/java/org/example/demoapp/service/ProductService.java
+++ src/main/java/org/example/demoapp/service/ProductService.java
@@ -1,50 +1,44 @@
 package org.example.demoapp.service;
 
 import org.example.demoapp.model.Product;
+import org.example.demoapp.repository.ProductRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PathVariable;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class ProductService {
 
-    private final List<Product> products = new ArrayList<>(List.of(
-            new Product(101, "SAMSUNG", 1_200_000),
-            new Product(12, "iPhone", 433_333)
-    ));
+    @Autowired
+    ProductRepo repository;
+
+//    private final List<Product> products = new ArrayList<>(List.of(
+//            new Product(101, "SAMSUNG", 1_200_000),
+//            new Product(12, "iPhone", 433_333)
+//    ));
 
     public List<Product> getProducts() {
-        return products;
+        return repository.findAll();
     }
 
     public Product getProductById(int id) {
-        return products.stream()
-                .filter(product -> product.getId() == id)
-                .findFirst()
+        return repository.findById(id)
                 .orElse(new Product(-1, "Not item found", 0));
     }
 
     public void addProduct(Product product) {
-        products.add(product);
+        repository.save(product);
     }
 
 
     public void updateProduct(Product productToUpdate) {
-        for (Product item : products) {
-            if (item.getId() == productToUpdate.getId()) {
-                products.set(products.indexOf(item), productToUpdate);
-            }
-        }
+        repository.save(productToUpdate);
 
     }
-        public void deleteProduct (int id){
-            boolean exist = products.removeIf(product -> product.getId() == id);
-            if (!exist) {
-                throw new IllegalArgumentException("Product ID: " + id + " Not Found...");
-            }
+
+    public void deleteProduct (int id){
+        repository.deleteById(id);
     }
 
 }
